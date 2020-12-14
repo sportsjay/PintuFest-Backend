253c08515ef9ef0acd0552aec7fb10e1488148a9
Jason Nathaniel <jasonnathaniel1152@gmail.com>
2020-12-14 16:20:26 +0800

    established nodemon server

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
 
+const PORT = 4000; 
+
+app.use(cors());
+app.use(bodyParser.json());
+
+dotenv.config();
+
+
+
+app.listen(PORT, function() {
+  console.log("Server is running on Port: " + PORT);
+});
